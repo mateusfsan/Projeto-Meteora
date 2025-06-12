cbd95448b3ca6b65f0a7d75ab1277293da7fb004
Mateus <fernandesmateus2002@gmail.com>
2025-06-12 14:07:40 -0300

    Correção de responsividade no carrosel 0.2

--- scripts.js/main.js
+++ scripts.js/main.js
@@ -4,11 +4,11 @@
 
   // Abrir e fechar ao clicar no botão
   btnMenu.addEventListener('click', (e) => {
-    e.stopPropagation(); // Impede o clique de fechar o menu imediatamente
+    e.stopPropagation();
     menu.classList.toggle('active');
   });
 
-  // Fechar ao clicar fora
+    //fechar ao clicar fora
   document.addEventListener('click', (e) => {
     if (!menu.contains(e.target) && !btnMenu.contains(e.target)) {
       menu.classList.remove('active');
@@ -19,4 +19,36 @@
   menu.addEventListener('click', (e) => {
     e.stopPropagation();
   });
+
+   function atualizarImagemCarrossel() {
+    const imagem1 = document.getElementById("slide1");
+    const imagem2 = document.getElementById("slide2");
+    const imagem3 = document.getElementById("slide3");
+    const larguraTela = window.innerWidth;
+
+    if (larguraTela <= 768) {
+  // Mobile
+  imagem1.src = "../assets/img/Banner_mobile.png";
+  imagem2.src = "../assets/img/COLEÇÃO ATEMPORAL_mobile.png";
+  imagem3.src = "../assets/img/COLEÇÃO ATEMPORAL2_mobile.png";
+
+} else if (larguraTela >= 769 && larguraTela <= 1024) {
+  // Tablet
+  imagem1.src = "../assets/img/Banner_tablet.png";
+  imagem2.src = "../assets/img/COLEÇÃO ATEMPORAL_tablet.png";
+  imagem3.src = "../assets/img/COLEÇÃO ATEMPORAL(1)_tablet.png";
+
+} else {
+  // Desktop
+  imagem1.src = "../assets/img/Banner_carousel.png";
+  imagem2.src = "../assets/img/COLEÇÃO ATEMPORAL-desktop.png";
+  imagem3.src = "../assets/img/COLEÇÃO ATEMPORAL (1)-desktop.png";
+}
+  }
+
+  // Executa quando a página carrega
+  window.addEventListener("load", atualizarImagemCarrossel);
+
+  // Executa quando a tela é redimensionada
+  window.addEventListener("resize", atualizarImagemCarrossel);
 
